e5e12d0f740aeb1f5a59bdb8a30513c263f13750
Ian Dang <iandddang@gmail.com>
2022-04-30 19:46:39 -0600

    executeCommand.js add about command

--- src/terminal/scripts/executeCommand.js
+++ src/terminal/scripts/executeCommand.js
@@ -27,6 +27,52 @@ const executeCommand = (commands, command, args) => {
 
   }
 
+  function about() {
+    let output = ''
+
+    output += '=========================================================================\n'
+    output += '==================================  ABOUT  ================================\n'
+    output += '=========================================================================\n'
+    output += 'Hi, my name is Ian Dang and thank you so much for visiting my website!\n'
+    output += 'I am a software engineer residing in Orange County, California.\n'
+    output += 'This is my portfolio, made in ReactJS, that I plan to update regularly.\n'
+    output += '=========================================================================\n'
+    output += '============================= TECHNOLOGIES ==============================\n'
+    output += '=========================================================================\n'
+    output += '======== LANGUAGES: Python 2 + 3, C, Java, JavaScript, Bash, SQL\n'
+    output += '======== FOCUSES: Automation, Scripting, Scraping, REST APIs\n'
+    output += '======== FOCUSES 2: Microservices, System Design/Architecture\n'
+    output += '======== DB: PostgreSQL, MongoDB\n'
+    output += '======== FRONTEND: Angular, React\n'
+    output += '======== OS: Linux, Windows, OSX\n'
+    output += '======== DEVOPS Tools: Jenkins, Docker, Prometheus, Grafana\n'
+    output += '======== DEVOPS Tools 2: NginX, Elastic Stack, AWS\n'
+    output += '======== PARADIGMS: Cloud Computing, Distributed Systems, CI/CD\n'
+    output += '======== HIPAA-COMPLIANT, PRODUCTION DEVOPS: ✔\n'
+    output += '===========================================================================\n'
+    output += '================================ WORK EXP =================================\n'
+    output += '===========================================================================\n'
+    output += '======== [Huntington Beach, CA] Voluware SWE 3 (2019 to XXXX)\n'
+    output += 'https://voluware.com\n...\n'
+    output += '\n======== [Newport Beach, CA] Zulu Quant SWE Intern (2018 to 2019)\n'
+    output += 'n/a\n'
+    output += '===========================================================================\n'
+    output += '================================ EDUCATION ================================\n'
+    output += '===========================================================================\n'
+    output += 'University of California, Irvine (2019)\n'
+    output += 'Computer Engineering, B.S.\n'
+    output += '===========================================================================\n'
+    output += '=================================== RESUME ================================\n'
+    output += '===========================================================================\n'
+    output += '======== TBD\n'
+    output += '===========================================================================\n'
+    output += '================================== CONTACT ================================\n'
+    output += '===========================================================================\n'
+    output += '======== LINKEDIN\n'
+    output += 'https://www.linkedin.com/in/ian-dang\n...\n'
+    return output
+  }
+
   function shrimp() {
 
     const shrimpAscii = `
@@ -64,12 +110,14 @@ const executeCommand = (commands, command, args) => {
     __________________________________________________________________
     `
 
-    return shrimpAscii + '\nhttps://www.instagram.com/aquamaynn/?hl=en'
+    return shrimpAscii + '\nAt the start of the pandemic, I stepped foot into the freshwater aquarium hobby!\n----> View my public collection of images/videos here...\nhttps://www.instagram.com/aquamaynn/?hl=en\n...\n'
   }
+
 
   const commandExecutionDict = {
     "help": help,
     "clear": clear,
+    "about": about,
     "shrimp": shrimp,
   }
 
